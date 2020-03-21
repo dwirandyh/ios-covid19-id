a392a61eaf4c559b9a2d72bfbe1db5c14805c8ca
Dwi Randy Herdinanto <dwirandyherdinanto@gmail.com>
2020-03-21 10:15:33 +0700

    Initialise project

---
+++ ios-covid19/ios-covid19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-covid19
+//
+//  Created by Dwi Randy Herdinanto on 21/03/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-covid19/ios-covid19Tests/ios_covid19Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_covid19Tests.swift
+//  ios-covid19Tests
+//
+//  Created by Dwi Randy Herdinanto on 21/03/20.
+//  Copyright © 2020 dwirandyh.com. All rights reserved.
+//
+
+import XCTest
+@testable import ios_covid19
+
+class ios_covid19Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
